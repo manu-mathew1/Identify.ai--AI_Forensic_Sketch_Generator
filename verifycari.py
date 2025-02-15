d2a556ce1a9879fd40a24e0e8ac1917fee746d45
Thomas Paul CJ <thomaspaulcj921@gmail.com>
2025-02-15 10:25:59 +0530

    Initial Push
    Initial Stage

---
+++ verifycari.py
@@ -0,0 +1,4 @@
+import os
+
+model_path = "C:/Users/cj/Documents/VScode/AI_Caricature_Designer/models/caricatureGAN"  # Update this to your actual path
+print("Model exists:", os.path.exists(model_path))
